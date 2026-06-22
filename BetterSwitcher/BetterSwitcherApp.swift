c5cb9aa5f3ac58d845a848b47369bf3cd1d41977
abdel-17 <abdelrahmanbahaa@proton.me>
2026-06-22 03:12:28 +0300

    Initial Commit

---
+++ BetterSwitcher/BetterSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSwitcherApp.swift
+//  BetterSwitcher
+//
+//  Created by Abdelrahman on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
