9c6a4734e298964c4035df78f973d0c891d44f92
abdel-17 <abdelrahmanbahaa@proton.me>
2026-06-23 09:08:17 +0300

    trigger using alt-tab

--- BetterSwitcher/BetterSwitcherApp.swift
+++ BetterSwitcher/BetterSwitcherApp.swift
@@ -1,23 +1,62 @@
 import AppKit
+import Carbon
 import SwiftUI
 
 @main
 struct BetterSwitcherApp: App {
 	let panel = ApplicationSwitcherPanel(applications: Applications())
 
+	init() {
+		let target = GetApplicationEventTarget()
+		var eventType = EventTypeSpec(
+			eventClass: OSType(kEventClassKeyboard),
+			eventKind: OSType(kEventHotKeyPressed),
+		)
+        var eventHandler: OpaquePointer?
+        let installStatus = InstallEventHandler(
+            target,
+            { _, _, userData in
+                let panel = Unmanaged<ApplicationSwitcherPanel>.fromOpaque(userData!).takeUnretainedValue()
+                panel.center()
+                panel.makeKeyAndOrderFront(nil)
+                return noErr
+            },
+            1,
+            &eventType,
+            Unmanaged.passRetained(panel).toOpaque(),
+            &eventHandler
+        )
+        guard installStatus == noErr else {
+            print("failed to install keyboard event handler", installStatus)
+            return
+        }
+
+        let hotKeyID = EventHotKeyID(signature: 0x42535453, id: 1)
+        var hotKey: OpaquePointer?
+        let registerStatus = RegisterEventHotKey(
+            UInt32(kVK_Tab),
+            UInt32(optionKey),
+            hotKeyID,
+            target,
+            0,
+            &hotKey
+        )
+        guard registerStatus == noErr else {
+            print("failed to register hotkey", registerStatus)
+            return
+        }
+	}
+
 	var body: some Scene {
 		MenuBarExtra("BetterSwitcher", systemImage: "arrow.right.arrow.left") {
-			Button("Open Switcher", action: openSwitcher)
-			Button("Quit", action: quit)
+			Button("Open Switcher") {
+				panel.center()
+				panel.makeKeyAndOrderFront(nil)
+			}
+
+			Button("Quit") {
+				NSApplication.shared.terminate(nil)
+			}
 		}
-	}
-
-	func openSwitcher() {
-		panel.center()
-		panel.makeKeyAndOrderFront(nil)
-	}
-
-	func quit() {
-		NSApplication.shared.terminate(nil)
 	}
 }
